64aa2b4403f02548b58c082ee7305d37c1c8982a
Tolkun Omurbekova <tolkun3922@gmail.com>
2023-12-21 15:39:31 +0600

    -рефакторинг

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,43 @@ import {LeftPanel} from "./layouts/LeftPanel/LeftPanel";
 import {Body} from "./layouts/Body/Body";
 import {Header} from "./components/Header/Header";
 import {CardList} from "./components/CardList/CardList";
-import {CardButton} from "./components/Card/CardButton/CardButton";
-import {Card} from "./components/Card/Card";
 import {CardAddButton} from "./components/CardAddButton/CardAddButton";
 import {CardForm} from "./components/CardForm/CardForm";
 import {useState} from "react";
 
 const initialData = [
 	{
-		id:1,
+		id: 1,
 		title: 'Подготовка к обновлению курсов ',
 		date: new Date(),
 		text: 'Горные походы открывают удивительные природные ландшафт'
 	},
 	{
-		id:2,
+		id: 2,
 		title: 'Поход в горы',
 		date: new Date(),
 		text: 'Думал что это очень много време...'
 	}
 ];
-function App() {
-	const [cards,setCards]=useState(initialData);
 
-	const addCard=(card)=>{
-		setCards(oldItems => [...oldItems,{
-			id:Math.max(...oldItems.map(item=>item.id))+1,
-			text:card.text,
-			title:card.title,
-			date:new Date(card.date)
+function App() {
+	const [cards, setCards] = useState(initialData);
+
+	const addCard = (card) => {
+		setCards(oldItems => [...oldItems, {
+			id: Math.max(...oldItems.map(item => item.id)) + 1,
+			text: card.text,
+			title: card.title,
+			date: new Date(card.date)
 		}]);
 	};
-	const sortItems=(a,b)=>{
-		if(a.date>b.date){
-			return 1;
-		}
-		else{
-			return -1;
-		}
-	};
+
 	return (
 		<div className='app'>
 			<LeftPanel>
 				<Header/>
 				<CardAddButton/>
-				<CardList>
-					{cards.sort(sortItems).map(item=>(
-						<CardButton key={item.id}>
-							<Card title={item.title} text={item.text} date={item.date.toDateString()}/>
-						</CardButton>
-					))}
-				</CardList>
+				<CardList cards={cards}/>
 			</LeftPanel>
 			<Body>
 				<CardForm onAddCardHandler={addCard}/>
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,9 +1,26 @@
 import s from './CardList.module.css';
+import {CardButton} from "../Card/CardButton/CardButton.jsx";
+import {Card} from "../Card/Card.jsx";
 
-export const CardList = ({children}) => {
+export const CardList = ({cards}) => {
+	if(cards.length==0){
+		return <p>Записей пока нет, добавьте новую!</p>;
+	}
+	const sortItems = (a, b) => {
+		if (a.date > b.date) {
+			return 1;
+		} else {
+			return -1;
+		}
+	};
 	return (
 		<div className={s.cardList}>
-			{children}
+			{
+				cards.sort(sortItems).map(item => (
+					<CardButton key={item.id}>
+						<Card title={item.title} text={item.text} date={item.date.toDateString()}/>
+					</CardButton>
+				))}
 		</div>
 	);
 };
