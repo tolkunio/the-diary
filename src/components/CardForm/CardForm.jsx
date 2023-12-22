5d44c6364d801cc573826a874f78189408905b8f
Tolkun Omurbekova <tolkun3922@gmail.com>
2023-12-22 14:58:13 +0600

    -css модули

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -1,22 +1,47 @@
 import s from './CardForm.module.css';
 import {Button} from "../Button/Button";
+import {useState} from "react";
 
 export const CardForm = ({onAddCardHandler}) => {
+	const [formValidState, setFormValidState] = useState({
+		title: true,
+		text: true,
+		date: true
+	});
+
 
 	const addNote = (event) => {
 		event.preventDefault();
 		const formData = new FormData(event.target);
 		const formProps = Object.fromEntries(formData);
+		let isFormValid = true;
+		{
+			console.log(formProps);}
+		if (!formProps.title?.trim().length) {
+			setFormValidState(state => ({...state, title: false}));
+			isFormValid=false;
+		}
+		if (!formProps.text?.trim().length) {
+			setFormValidState(state => ({...state, text: false}));
+			isFormValid=false;
+		}
+		if (!formProps.date?.trim().length) {
+			setFormValidState(state => ({...state, title: false}));
+			isFormValid=false;
+		}
+		if (isFormValid==false) {
+			return;
+		}
 		onAddCardHandler(formProps);
 	};
 
 	return (
 		<form className={s.cardForm} onSubmit={addNote}>
-			<input type={"text"} name={'title'}/>
-			<input type={"date"} name={'date'}/>
-			<input type={"text"}  name={'text'}/>
+			<input type={"text"} name={'title'} className={`${formValidState.title ? '' : s.invalid}`}/>
+			<input type={"date"} name={'date'} className={`${formValidState.date ? '' : s.invalid}`}/>
+			<input type={"text"} name={'text'} className={`${formValidState.text ? '' : s.invalid}`}/>
 			<textarea name={'post'} id={''} cols={'30'} rows={'10'}></textarea>
-			<Button text={'Save'} onClick={()=>{console.log('pressed');}}/>
+			<Button text={'Save'}/>
 		</form>
 	);
 };
