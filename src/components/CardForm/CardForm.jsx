704df12c3ef58f918ce2f3057b8171be4f35575d
Tolkun Omurbekova <tolkun3922@gmail.com>
2023-12-27 11:59:18 +0600

    -added library cn,svg icons

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -1,6 +1,7 @@
 import s from './CardForm.module.css';
 import {Button} from "../Button/Button";
 import {useState} from "react";
+import cn from 'classnames';
 
 export const CardForm = ({onAddCardHandler}) => {
 	const [formValidState, setFormValidState] = useState({
@@ -16,20 +17,21 @@ export const CardForm = ({onAddCardHandler}) => {
 		const formProps = Object.fromEntries(formData);
 		let isFormValid = true;
 		{
-			console.log(formProps);}
+			console.log(formProps);
+		}
 		if (!formProps.title?.trim().length) {
 			setFormValidState(state => ({...state, title: false}));
-			isFormValid=false;
+			isFormValid = false;
 		}
 		if (!formProps.text?.trim().length) {
 			setFormValidState(state => ({...state, text: false}));
-			isFormValid=false;
+			isFormValid = false;
 		}
 		if (!formProps.date?.trim().length) {
 			setFormValidState(state => ({...state, title: false}));
-			isFormValid=false;
+			isFormValid = false;
 		}
-		if (isFormValid==false) {
+		if (isFormValid == false) {
 			return;
 		}
 		onAddCardHandler(formProps);
@@ -37,9 +39,24 @@ export const CardForm = ({onAddCardHandler}) => {
 
 	return (
 		<form className={s.cardForm} onSubmit={addNote}>
-			<input type={"text"} name={'title'} className={`${formValidState.title ? '' : s.invalid}`}/>
-			<input type={"date"} name={'date'} className={`${formValidState.date ? '' : s.invalid}`}/>
-			<input type={"text"} name={'text'} className={`${formValidState.text ? '' : s.invalid}`}/>
+			<div>
+				<input type={"text"} name={'title'} className={cn(s.invalid, s.inputTitle)}/>
+			</div>
+			<div className={s.formRow}>
+				<label className={s.formLabel} htmlFor={'date'}>
+					<img src={'/calendar.svg'} alt={'calendar'}/>
+					<span>Дата</span>
+				</label>
+				<input id='date' type={"date"} name={'date'} className={cn(s.input,`${formValidState.date ? '' : s.invalid}`)}/>
+			</div>
+			<div className={s.formRow}>
+				<label className={s.formLabel} htmlFor={'text'}>
+					<img src={'/folder.svg'} alt={'folder'}/>
+					<span>Метки</span>
+				</label>
+				<input type={"text"} name={'text'} id={'text'} className={`${s.input}${formValidState.text ? '' : s.invalid}`}/>
+			</div>
+
 			<textarea name={'post'} id={''} cols={'30'} rows={'10'}></textarea>
 			<Button text={'Save'}/>
 		</form>
